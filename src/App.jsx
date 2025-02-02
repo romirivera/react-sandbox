f73093c7e7ef5be252bf42314650edc2f59bd2a4
Romina Rivera <rdrivera@uc.cl>
2025-02-02 03:09:22 -0300

    todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,49 @@
 import './App.css';
-import ListaUsuarios from './listaUsuarios';
-import NavBar from './navbar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Home';
-import About from './About';
-import Contact from './Contact';
-import Error404 from './Error404';
+import { useState } from 'react';
+
+// <p onClick={}>Hola</p> es equivalente a
+// React.createElement('p', {onClick: () => {}}, 'Hola')
 
 function App() {
+  const [todos, setTodos] = useState(['enseñar react', 'abrazar a rasita', 'dormir']);
+
+  const [newTodo, setNewTodo] = useState('');
+
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/usuarios' element={<ListaUsuarios />} />
-        <Route path='*' element={<Error404 />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <ul>
+        {todos.map((todo) => {
+          return <li key={todo}>{todo}</li>;
+        })}
+      </ul>
+
+      <form>
+        <label htmlFor='new-todo'>Nuevo pendiente (todo)</label>
+
+        <input
+          id='new-todo'
+          type='text'
+          value={newTodo}
+          onChange={(event) => setNewTodo(event.currentTarget.value)}
+        />
+
+        <button
+          type='submit'
+          onClick={(event) => {
+            event.preventDefault();
+
+            console.log('todos antes: ', todos);
+
+            setTodos([...todos, newTodo]);
+
+            setNewTodo('');
+          }}
+        >
+          crear
+        </button>
+      </form>
+    </div>
   );
 }
-export default App;
 
-/*export default App;*/
+export default App;
