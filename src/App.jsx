8f9cbf6bcea1ff58b0c82218a6080b3c233ef088
Romina Rivera <rdrivera@uc.cl>
2025-02-02 03:23:40 -0300

    extraer en components

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,63 @@
+/* eslint-disable react/prop-types */
 import './App.css';
 import { useState } from 'react';
 
 // <p onClick={}>Hola</p> es equivalente a
 // React.createElement('p', {onClick: () => {}}, 'Hola')
 
-function App() {
-  const [todos, setTodos] = useState(['enseñar react', 'abrazar a rasita', 'dormir']);
+function TodoList(props) {
+  return (
+    <ul>
+      {props.todos.map((todo) => {
+        return <li key={todo}>{todo}</li>;
+      })}
+    </ul>
+  );
+}
 
+function TodoForm(props) {
   const [newTodo, setNewTodo] = useState('');
 
   return (
+    <form>
+      <label htmlFor='new-todo'>Nuevo pendiente (todo)</label>
+
+      <input
+        id='new-todo'
+        type='text'
+        value={newTodo}
+        onChange={(event) => setNewTodo(event.currentTarget.value)}
+      />
+
+      <button
+        type='submit'
+        onClick={(event) => {
+          event.preventDefault();
+
+          props.onTodoCreado(newTodo);
+
+          setNewTodo('');
+        }}
+      >
+        crear
+      </button>
+    </form>
+  );
+}
+
+function App() {
+  const [todos, setTodos] = useState(['enseñar react', 'abrazar a rasita', 'dormir']);
+
+  return (
     <div>
-      <ul>
-        {todos.map((todo) => {
-          return <li key={todo}>{todo}</li>;
-        })}
-      </ul>
+      <TodoList todos={todos} />
 
-      <form>
-        <label htmlFor='new-todo'>Nuevo pendiente (todo)</label>
-
-        <input
-          id='new-todo'
-          type='text'
-          value={newTodo}
-          onChange={(event) => setNewTodo(event.currentTarget.value)}
-        />
-
-        <button
-          type='submit'
-          onClick={(event) => {
-            event.preventDefault();
-
-            console.log('todos antes: ', todos);
-
-            setTodos([...todos, newTodo]);
-
-            setNewTodo('');
-          }}
-        >
-          crear
-        </button>
-      </form>
+      <TodoForm
+        onTodoCreado={(newTodo) => {
+          console.log('todo creado: ', newTodo);
+          setTodos([...todos, newTodo]);
+        }}
+      />
     </div>
   );
 }
